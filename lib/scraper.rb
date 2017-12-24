b71ef2bab2ee9fa41f73ed13bc802ffd825e8cb8
Jawann <jawann@gmail.com>
2017-12-24 06:52:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,19 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = File.read('fixtures/student-site/index.html')
+    scraper = Nokogiri::HTML(html)
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+#{ }`:name`, `:location` and `:profile_url`
